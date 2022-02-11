656cd5b13f48f4a211645793d874b6a8f423e1a1
Gustavo França <gstvz.franca@gmail.com>
2022-02-11 09:58:02 -0300

    feat: created resetPassword spec, to test an unauthorized user trying to reset a password and an authorized one resetting it

---
+++ cypress/integration/resetPassword.js
@@ -0,0 +1,25 @@
+import { existingUser, url } from "../support/";
+
+describe('User Reset Password', () => {
+  const { email, password } = existingUser;
+  beforeEach(() => {
+    cy.visit(`${url}/auth/reset`);
+  });
+
+  it.skip('Should not send link to unauthorized user.', () => {
+    cy.resetPassword('unauthorized@unauthorized.com', 404, 'Request failed with status code 404');
+  });
+
+  it('Should send link to authorized user and reset password.', () => {
+    cy.resetPassword(email, 200, 'Link sent');
+
+    cy.intercept("POST", "**/reset/**").as("resetToken");
+    cy.get('#password').type(password);
+
+    cy.get('.sc-iJKOTD').click();
+    cy.wait("@resetToken").then((xhr) => {
+      expect(xhr.response.statusCode).be.eq(200);
+    });
+    cy.contains('Password reset').should('be.visible');
+  });
+})
